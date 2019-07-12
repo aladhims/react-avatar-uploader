9ef5e3f841823f0d00b38b125cc60b8d6499fe61
WhizSid <whizsid@aol.com>
2019-07-13 01:11:43 +0530

    Typescript support

---
+++ src/types.d.ts
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { AxiosResponse } from "axios";
+
+export interface Props {
+  uploadURL: string;
+  name: string;
+  onFinished?: (
+    err: boolean | Error,
+    res?: Promise<AxiosResponse<any>>
+  ) => void;
+  onStart?: () => void;
+  onProgress?: (percent: number) => void;
+  placeholder?: string;
+  withCredentials?: boolean;
+  customHeaders?: { [x: string]: string };
+  disabled?: boolean;
+  fileType?: string;
+  size?: number;
+  defaultImg: string;
+}
+
+declare class AvatarUploader extends React.Component<Props> {
+  constructor(props: Props);
+}
+
+export default AvatarUploader;
